f80c5c2ea41825ac03a9a63c96e9564db540ce17
Shaun Rodgers <52825780+sciados@users.noreply.github.com>
2025-12-09 16:17:37 +0000

    feat: Updated the Text2Video Ai Platforms 23.0

--- 034_add_error_code.sql
+++ 034_add_error_code.sql
@@ -1,18 +1,7 @@
 -- Migration 034: Add error_code column to video_generations
 -- This SQL can be run directly on the database
 
--- Add error_code column if it doesn't exist
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM information_schema.columns
-        WHERE table_name = 'video_generations'
-        AND column_name = 'error_code'
-    ) THEN
-        ALTER TABLE video_generations ADD COLUMN error_code VARCHAR(50);
-        RAISE NOTICE 'Added error_code column to video_generations table';
-    ELSE
-        RAISE NOTICE 'error_code column already exists, skipping';
-    END IF;
-END
-$$;
+-- Add error_code column if it doesn't exist (PostgreSQL 10+)
+ALTER TABLE video_generations ADD COLUMN IF NOT EXISTS error_code VARCHAR(50);
+
+-- Note: This is safe to run multiple times
