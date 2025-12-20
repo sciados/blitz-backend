f257fa4fa7d47bfa44874c3f05a9beaacc0455c6
Shaun Rodgers <52825780+sciados@users.noreply.github.com>
2025-12-20 11:54:30 +0000

    Force Railway redeploy 3.0

--- 034_add_error_code.sql
+++
@@ -1,7 +0,0 @@
--- Migration 034: Add error_code column to video_generations
--- This SQL can be run directly on the database
-
--- Add error_code column if it doesn't exist (PostgreSQL 10+)
-ALTER TABLE video_generations ADD COLUMN IF NOT EXISTS error_code VARCHAR(50);
-
--- Note: This is safe to run multiple times
